930019cb01797d50c51bb7bef66c2c89768cea4a
Nir Adler <nira7008@gmail.com>
2022-01-24 20:50:28 +0200

    update

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const express=require('express')
-const app=express()
+// const express = require('express')
+// const app = express()
+const csv = require('csvtojson')
+const csvFilePath = './nasdaq.csv'
+const fs = require('fs')
 
+const csvToJson = async () => {
+    try {
+        const jsonArray = await csv().fromFile(csvFilePath)
+        let filterJson = jsonArray.map((stock) => {
+            if (stock.Name.toLowerCase().includes('common stock')) {
+                return { Name: stock.Name, Symbol: stock.Symbol }
+            }
+        })
+        console.log(filterJson)
+        fs.writeFileSync('./data.json', JSON.stringify(filterJson))
+    } catch (e) {
+        console.log(e)
+    }
+}
 
+csvToJson()
 
-
-
-
-app.listen(8081,()=>{
-    console.log('server up on port 8081')
-})
+// app.listen(8081, () => {
+//     console.log('server up on port 8081')
+// })
 
 
 
